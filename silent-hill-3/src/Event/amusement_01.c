2f0adaafddcaeb44d98b5f2d3f1839af2d67901c
moth dreams <mothdreams666@gmail.com>
2026-03-06 00:47:17 -0800

    sh3: add 9 `amusement_01` functions

--- silent-hill-3/src/Event/amusement_01.c
+++ silent-hill-3/src/Event/amusement_01.c
@@ -1,24 +1,131 @@
-#include "common.h"
+#include "amusement_01.h"
 
+#ifdef NON_MATCHING
+int func_01F6D680_amusement_01() {
+    int ret;
+
+    // @todo is there a cleaner match?
+    switch (D_01F74C88_amusement_01)
+        case 0: {
+            func_001C2290(2, 0.8f);
+            D_01F74C98_amusement_01 = 45.0f;
+            D_01F74CA0_amusement_01 = 150.0f;
+            D_01F74C88_amusement_01++;
+        }
+
+    ret = func_0016C540(&D_01F74510_amusement_01, &D_01F74570_amusement_01);
+    func_01F703B0_amusement_01(D_01F74C98_amusement_01, D_01F74CA0_amusement_01);
+
+    switch (D_01F74C88_amusement_01)
+        case 1:
+            if (func_001643C0() > 438.0f) {
+                D_01F74C98_amusement_01 = 50.0f;
+                D_01F74CA0_amusement_01 = 100.0f;
+                D_01F74C88_amusement_01++;
+            }
+
+    if (ret != 0) {
+        D_01F74C88_amusement_01 = 0;
+    }
+
+    return ret;
+}
+#else
 INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D680_amusement_01);
+#endif
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D7A0_amusement_01);
+void func_01F6D7A0_amusement_01() {
+    func_0016C540(&D_01F74590_amusement_01, &D_01F745F0_amusement_01);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D7C0_amusement_01);
+int func_01F6D7C0_amusement_01() {
+    int audioIsPlaying;
+
+    D_1D3169C |= 0x1000;
+
+    // noping this function prevents the audio from playing
+    audioIsPlaying = func_0016CF80(12, 50, &D_01F713D0_amusement_01);
+
+
+    if (audioIsPlaying != 0) {
+        func_0016CF70();
+        D_1D3169C &= ~0x1000;
+    }
+
+    return audioIsPlaying;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D840_amusement_01);
+    
+void func_01F6DC40_amusement_01() {
+    func_001C0EB0(func_01F6DC70_amusement_01, 90, 1);
+    D_01F74CB0_amusement_01 = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DC40_amusement_01);
+static void func_01F6DC70_amusement_01(__int128* arg0, int* arg1, __int128* arg2, int* arg3, int* arg4) {
+    qcopy(arg0, &D_01F74680_amusement_01);
+    *arg1 = 1;
+    qcopy(arg2, &D_01F74670_amusement_01);
+    *arg3 = 0;
+    *arg4 = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DC70_amusement_01);
+void func_01F6DCB0_amusement_01() {
+    func_001C0EB0(func_01F6DCF0_amusement_01, 999999, 1);
+    D_01F74CB0_amusement_01 = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DCB0_amusement_01);
+void func_01F6DCF0_amusement_01(__int128* arg0, s32* arg1, __int128* arg2, s32* arg3, s32* arg4) {
+    sceVu0FVECTOR sp60;
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DCF0_amusement_01);
+    func_0018FE60((__int128*) &sp60);
+    sp60[1] += -700.0f;
+    qcopy(arg0, &D_01F74690_amusement_01[D_01F74CB0_amusement_01]);
+    *arg1 = 1;
+    if (D_01F74CB0_amusement_01 != 2) {    
+        qcopy(arg2, sp60);
+    } else {
+        qcopy(arg2, D_01F74670_amusement_01);
+    }
+    *arg3 = 1;
+    *arg4 = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DDC0_amusement_01);
+void func_01F6DDC0_amusement_01(void) {
+    D_01F74CB0_amusement_01++;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DDE0_amusement_01);
+int func_01F6DDE0_amusement_01() {
+    float limit;
+    int ret = 0;
+    int i;
+    float* cur;
+    int state = D_01F74C88_amusement_01;
+
+    switch (state) {
+        case 0:
+            func_01F6DC40_amusement_01();
+            D_01F74D98_amusement_01 = 0;
+            D_01F74C88_amusement_01++;
+            /* fallthrough */
+        case 1:
+            for (i = 0; cur = D_01F74610_amusement_01[i], cur[0] != 0.0f; i++) {
+                limit = D_01F74D98_amusement_01;
+                if (cur[0] <= limit && cur[1] > limit) {
+                    func_0016CA40(1);
+                }
+            }
+            D_01F74D98_amusement_01++;
+            if (D_01F74D98_amusement_01 < 90) {
+                break;
+            }
+        case 2:
+            ret = 1;
+            D_01F74C88_amusement_01 = 0;
+    }
+
+    return ret;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DF10_amusement_01);
 
---
+++ silent-hill-3/src/Event/amusement_01.h
@@ -0,0 +1,39 @@
+#ifndef AMUSEMENT_01_H
+#define AMUSEMENT_01_H
+
+#include "common.h"
+
+int func_01F6D680_amusement_01();
+
+void func_001C0EB0(void (*)(__int128*, int*, __int128*, int*, int*), int, int);
+
+void func_01F6DCF0_amusement_01(__int128* arg0, int* arg1, __int128* arg2, int* arg3, int* arg4);
+
+static void func_01F6DC70_amusement_01(__int128* arg0, int* arg1, __int128* arg2, int* arg3, int* arg4);
+
+extern float func_001643C0();
+extern int func_0016C540(float *, float *);
+extern void func_001C2290(int, float);
+extern void func_0018FE60(__int128*);
+extern void func_0016CF70();
+extern int func_0016CF80(int, int, int*);
+extern int D_01F713D0_amusement_01;
+extern int D_1D3169C;
+
+extern sceVu0FVECTOR D_01F74670_amusement_01;
+extern sceVu0FVECTOR D_01F74690_amusement_01[];
+extern float D_01F74610_amusement_01[][2];
+extern int D_01F74D98_amusement_01;
+extern int D_01F74CB0_amusement_01;
+extern __int128 D_01F74680_amusement_01;
+extern int D_01F74CB0_amusement_01;
+
+extern float D_01F74590_amusement_01;
+extern float D_01F745F0_amusement_01;
+extern float D_01F74510_amusement_01;
+extern float D_01F74570_amusement_01;
+extern int D_01F74C88_amusement_01;
+extern float D_01F74C98_amusement_01;
+extern float D_01F74CA0_amusement_01;
+
+#endif
