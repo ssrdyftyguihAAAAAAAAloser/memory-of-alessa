747482117b279fc0544468623bc44c4b3f4f4d92
moth dreams <mothdreams666@gmail.com>
2026-03-05 21:27:54 -0800

    sh3: move slurper x code to c

---
+++ silent-hill-3/src/Enemy/en_lix.c
@@ -0,0 +1,21 @@
+#include "Chacter/en_common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030DB50);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030DE10);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030DED0);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030DF30);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030DFC0);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030E1E0);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030E250);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030E2C0);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_0030E330);
+
+INCLUDE_ASM("asm/nonmatchings/Enemy/en_lix", func_00310170);
