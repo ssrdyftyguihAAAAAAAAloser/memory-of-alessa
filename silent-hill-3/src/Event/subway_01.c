ba1e7f657325ca7a3ca2f5ded3e7301425770d96
Daniel Freitas <101065507+Dandiels@users.noreply.github.com>
2026-03-28 15:36:14 -0300

    sh3: match func_01F6D690_subway_01 (#71)
    * add subway_01.h
    
    * match func_01F6D680_subway_01
    
    * update subway_01.h
    
    * match func_01F6D690_subway_01
    
    Co-authored-by: matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
    
    * update subway_01.h
    
    * match func_01F6E270_subway_01
    
    * Apply suggestion from @dreamingmoths
    
    Co-authored-by: dreamingmoths <mothdreams666@gmail.com>
    
    * Refactor subway_01
    
    * Change s32 to int in subway_01.h
    
    ---------
    
    Co-authored-by: matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
    Co-authored-by: dreamingmoths <mothdreams666@gmail.com>

--- silent-hill-3/src/Event/subway_01.c
+++ silent-hill-3/src/Event/subway_01.c
@@ -1,11 +1,33 @@
 #include "subway_01.h"
+#include "Chacter/m3_sc.h"
 
-s32 func_01F6D680_subway_01(void)
+int func_01F6D680_subway_01(void)
 {
     return 0x1;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6D690_subway_01);
+int func_01F6D690_subway_01(void)
+{
+    int var_s0 = 0;
+    
+    switch (D_01F70700_subway_01)
+    {
+        case 0:
+            func_00190A20(2);
+            D_01F70700_subway_01 += 1;
+        case 1:
+            if (func_0016CED0(0x4F, 0x1D, &D_01F70370_subway_01) != 0)
+            {
+                func_00190A20(0);
+                var_s0 = 1;
+                D_01F70700_subway_01 = 0;
+            }
+            
+            break;
+    }
+    
+    return var_s0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6D720_subway_01);
 
@@ -19,11 +41,19 @@ INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6DF90_subway_01);
 
 INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E100_subway_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E270_subway_01);
+void func_01F6E270_subway_01(void)
+{
+    int id;
+    int* var_s0 = &D_01F70650_subway_01;
 
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E2D0_subway_01);
-
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E320_subway_01);
+    D_1D3165C |= 0x8000;
+    
+    while (id = *var_s0)
+    {
+        shCharacter_Manage_Delete(DOUBLE_HEAD_CHARA_ID, id);
+        var_s0++;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E380_subway_01);
 
--- silent-hill-3/src/Event/subway_01.h
+++ silent-hill-3/src/Event/subway_01.h
@@ -3,4 +3,12 @@
 
 #include "common.h"
 
+int func_0016CED0(int, int, int*);
+int func_00190A20(int);
+
+extern int D_01F70370_subway_01;
+extern int D_01F70700_subway_01;
+extern int D_01F70650_subway_01;
+extern int D_1D3165C;
+
 #endif
