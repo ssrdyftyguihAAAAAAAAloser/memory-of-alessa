46df014fdd14bbaf1f80f6e0c1c84b37bbd2ed96
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-04 22:52:04 +0200

    sh2: match sh_character_status (#93)
    * update SLUS_202.28.yaml
    
    * Add files via upload
    
    * Update silent-hill-2/src/Chacter/sh_character_status.c

---
+++ silent-hill-2/src/Chacter/sh_character_status.c
@@ -0,0 +1,69 @@
+#include "sh_character_status.h"
+
+static void shBattleCheckHitEyes(struct _CL_VHIT_RESULT *eye, struct SubCharacter *scp, int i, int net) {
+    float sp[4];
+    float ep[4];
+
+    sp[0] = scp->pos.x;
+    sp[1] = scp->eye_y;
+    sp[2] = scp->pos.z;
+
+    if ((sh2_target_info[i].adr.scp->kind >> 8) == 7) {
+        ep[0] = sh2_target_info[i].adr.scp->pos_spd.x;
+        ep[1] = sh2_target_info[i].adr.scp->center_y;
+        ep[2] = sh2_target_info[i].adr.scp->pos_spd.z;
+    } else {
+        ep[0] = sh2_target_info[i].adr.scp->pos.x;
+        ep[1] = sh2_target_info[i].adr.scp->center_y;
+        ep[2] = sh2_target_info[i].adr.scp->pos.z;
+    }
+
+    clCheckHitEyes(eye, (u_int) scp, sp, ep, net);
+}
+
+void shBattleCheckTargetMyArea(struct shInArea *in_area, struct SubCharacter *scp, struct SubCharacter *tgt, float *look, float *feel) {
+    float tgt_pos[4];
+    float tgt_to_look;
+    float tgt_to_feel;
+
+    if ((tgt->kind >> 8) == 7) {
+        tgt_pos[0] = tgt->pos_spd.x;
+        tgt_pos[1] = tgt->center_y;
+        tgt_pos[2] = tgt->pos_spd.z;
+    } else {
+        tgt_pos[0] = tgt->pos.x;
+        tgt_pos[1] = tgt->center_y;
+        tgt_pos[2] = tgt->pos.z;
+    }
+
+    tgt_to_look = _shLength(look, tgt_pos);
+    tgt_to_feel = _shLength(feel, tgt_pos);
+
+    in_area->light_on = (sh2gfw_Check_CharaDarkOrBright(tgt) && tgt_to_feel <= 1.5f * scp->battle.look.radius) ? 1 : 0;
+    
+    in_area->look_on = (tgt_to_look <= scp->battle.look.radius) ? 1 : 0;
+    in_area->feel_on = (tgt_to_feel <= scp->battle.feel.radius) ? 1 : 0;
+
+    switch (tgt->kind >> 8) {
+        case 7:
+            if (!(scp->status & 0x200)) {
+                in_area->look_on = in_area->feel_on = 0;
+            }
+            if (tgt_pos[1] < scp->eye_y - 600.0f || tgt_pos[1] > scp->eye_y + 1000.0f) {
+                
+                in_area->light_on = in_area->look_on = in_area->feel_on = 0;
+            }
+            break;
+        case 2:
+            if (tgt->battle.dead_timer <= 1.5f) {
+                return;
+            }
+            in_area->light_on = in_area->look_on = in_area->feel_on = 0;;
+            break;
+        case 1:
+            in_area->light_on = in_area->feel_on = 0;
+            break;
+        default:
+            break;
+    }
+}
---
+++ silent-hill-2/src/Chacter/sh_character_status.h
@@ -0,0 +1,30 @@
+#ifndef SH_CHARACTER_STATUS_H
+#define SH_CHARACTER_STATUS_H
+
+#include "common.h"
+#include "Chacter/character.h"
+
+// total size: 0x1
+struct shInArea {
+    // Members
+    unsigned char look_on : 2; // offset 0x0, size 0x1
+    unsigned char feel_on : 2; // offset 0x0, size 0x1
+    unsigned char light_on : 4; // offset 0x0, size 0x1
+};
+
+// total size: 0xC
+struct shInAreaTgtInfo {
+    // Members
+    union /* @anon4 */ {
+        struct SubCharacter * scp; // offset 0x0, size 0x4
+    } adr; // offset 0x0, size 0x4
+    float distance; // offset 0x4, size 0x4
+    struct shInArea in_area; // offset 0x8, size 0x1
+};
+
+void clCheckHitEyes(struct _CL_VHIT_RESULT * res /* r2 */, u_int id /* r2 */, float * st /* r2 */, float * ed /* r2 */, int thru /* r2 */);
+struct shInAreaTgtInfo sh2_target_info[20];
+static float _shLength(float * v0 /* r2 */, float * v1 /* r2 */);
+int sh2gfw_Check_CharaDarkOrBright(void * SubChara /* r2 */);
+
+#endif
