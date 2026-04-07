939ea55977daf10c0132cbf445dc86b57056f6d9
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-07 06:59:15 +0200

    sh3: match 6 church_01 funcs (#96)
    * match 6 church_01 funcs
    
    * add church_01.h
    
    * Apply suggestions from code review

--- silent-hill-3/src/Event/church_01.c
+++ silent-hill-3/src/Event/church_01.c
@@ -1,20 +1,180 @@
-#include "common.h"
+#include "church_01.h"
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6D680_church_01);
+int func_01F6D680_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_0016CD00(&D_01F70280_church_01);
+            func_001C2290(3, 0.8f);
+            D_01F70600_church_01 = 1;
+            /* fallthrough */
+        case 1:
+            if (func_001C2580(2) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 2;
+            func_001C2290(5, 0.8f);
+        case 2:
+            if (func_0016BED0(0xCD, 0x49) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 3;
+            func_001C2290(5, 0.8f);
+        case 3:
+            func_00190A20(0);
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6D790_church_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6DA80_church_01);
+int func_01F6DA80_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_001C2290(3, 0.8f);
+            D_01F70600_church_01 = 1;
+            /* fallthrough */
+        case 1:
+            if (func_001C2580(2) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 2;
+            func_001C2290(5, 0.8f);
+        case 2:
+            if (func_0016BED0(0x59, 0x44) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 3;
+            func_001C2290(5, 0.8f);
+        case 3:
+            if (func_001C2580(4) == 0) {
+                return 0;
+            }
+            func_00190A20(0);
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6DBA0_church_01);
+int func_01F6DBA0_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_001C2290(3, 0.8f);
+            D_01F70600_church_01 = 1;
+            /* fallthrough */
+        case 1:
+            if (func_001C2580(2) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 2;
+            func_001C2290(5, 0.8f);
+        case 2:
+            if (func_0016BED0(0x5B, 0x46) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 3;
+            func_001C2290(5, 0.8f);
+        case 3:
+            if (func_001C2580(4) == 0) {
+                return 0;
+            }
+            func_00190A20(0);
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6DCC0_church_01);
+int func_01F6DCC0_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_001C2290(3, 0.8f);
+            D_01F70600_church_01 = 1;
+            /* fallthrough */
+        case 1:
+            if (func_001C2580(2) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 2;
+            func_001C2290(5, 0.8f);
+        case 2:
+            if (func_0016BED0(0x5A, 0x47) == 0) {
+                return 0;
+            }
+            D_01F70600_church_01 = 3;
+            func_001C2290(5, 0.8f);
+        case 3:
+            if (func_001C2580(4) == 0) {
+                return 0;
+            }
+            func_00190A20(0);
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6DDE0_church_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6DF40_church_01);
+int func_01F6DF40_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_0016CD00(&D_01F703F0_church_01);
+            D_01F70600_church_01++;
+            /* fallthrough */
+        case 1:
+            if (!GET_FLAG(D_1D316A8, 0x15)) {
+                if (func_0016C1C0(0x5F) == 0) {
+                    return 0;
+                }
+                func_0016C3C0();
+            }
+            D_01F70600_church_01++;
+        case 2:
+            if (func_0016BED0(0x1C, 0x5E) == 0) {
+                return 0;
+            }
+            func_00190A20(0);
+            D_1D316A8[0] |= 0x200000;
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6E040_church_01);
+int func_01F6E040_church_01(void) {
+    switch (D_01F70600_church_01) {
+        case 0:
+            func_00190A20(2);
+            func_0016CD00(&D_01F70400_church_01);
+            D_01F70600_church_01++;
+            /* fallthrough */
+        case 1:
+            if (!GET_FLAG(D_1D316A8, 0x16)) {
+                if (func_0016C1C0(0x61) == 0) {
+                    return 0;
+                }
+                func_0016C3C0();
+            }
+            D_01F70600_church_01++;          
+        case 2:
+            if (func_0016BED0(0x1B, 0x60) == 0) {
+                return 0;
+            }
+            func_00190A20(0);
+            D_1D316A8[0] |= 0x400000;
+        default:
+            D_01F70600_church_01 = 0;
+            return 1;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/church_01", func_01F6E140_church_01);
 
---
+++ silent-hill-3/src/Event/church_01.h
@@ -0,0 +1,19 @@
+#ifndef CHURCH_01_H
+#define CHURCH_01_H
+
+#include "common.h"
+
+int func_0016BED0(int, int);
+void func_0016CD00(int *);
+int func_00190A20(int);
+void func_001C2290(int, float);
+int func_001C2580(int);
+int func_0016C1C0(int);
+void func_0016C3C0(void);
+extern int D_01F70280_church_01;
+extern int D_01F70400_church_01;
+extern int D_01F70600_church_01;
+extern int D_01F703F0_church_01;
+extern u_int D_1D316A8[];
+
+#endif
