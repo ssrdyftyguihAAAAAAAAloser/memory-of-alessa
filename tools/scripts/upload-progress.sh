074ed37b2c06dfef864429eca3027c6e7db716ea
moth dreams <mothdreams666@gmail.com>
2026-04-11 14:02:26 -0700

    update progress actions

--- tools/scripts/upload-progress.sh
+++ tools/scripts/upload-progress.sh
@@ -2,10 +2,12 @@
 
 # this script runs in the GitHub action to upload the report to silenthillmuseum.org.
 
-gzip build/$SERIAL/report.json
-curl -f \
+gzip --keep build/$SERIAL/report.json
+report_status=$(curl -f \
     -X POST "https://silenthillmuseum.org:666/upload-progress?serial=$SERIAL" \
     -H "Content-Type: application/json" \
     -H "Content-Encoding: gzip" \
     -H "x-api-key: $PROGRESS_API_KEY" \
-    --data-binary @build/$SERIAL/report.json.gz
+    --data-binary @build/$SERIAL/report.json.gz)
+echo "report status: $report_status"
+echo "REPORT_STATUS=$report_status" >> "$GITHUB_ENV"
