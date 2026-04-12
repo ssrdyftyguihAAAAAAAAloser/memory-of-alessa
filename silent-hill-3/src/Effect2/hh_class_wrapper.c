d5a0a5d49cd269b794c8044d2b07e4fe91e81c7b
moth dreams <mothdreams666@gmail.com>
2026-04-11 19:45:52 -0700

    sh3: migrate `hh_class_wrapper` functions

--- silent-hill-3/src/Effect2/hh_class_wrapper.c
+++ silent-hill-3/src/Effect2/hh_class_wrapper.c
@@ -1,4 +1,10 @@
-#include "common.h"
+#include "Effect2/hh_class_wrapper.h"
+
+static void ViewFrustum_Primitive_ClipMatrix_Create(void);
+static void ViewFrustum_BoundingBox_ClipMatrix_Create(void);
+static void AlwaysFront_WorldView_Matrix_Create(void);
+
+extern /* static */ HH_Class_Wrapper_Work * _pWork;
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", ViewFrustum_Primitive_ClipMatrix_Create);
 
@@ -14,7 +20,16 @@ INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142A00);
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_Point_Clip_Judge);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B10);
+void HH_ClassWrapper_Matrix_Group_Update(void) {
+    
+    ViewFrustum_Primitive_ClipMatrix_Create();
+    
+    
+    ViewFrustum_BoundingBox_ClipMatrix_Create();
+    
+    
+    AlwaysFront_WorldView_Matrix_Create();
+}
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_ViewFrustum_Primitive_ClipMatrix_Get);
 
@@ -22,21 +37,24 @@ INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B50);
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B60);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B70);
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_WorldViewMatrix_Get);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B80);
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_ViewScreenMatrix_Get);
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142B90);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142BA0);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142BB0);
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_ViewingFrustumParamerter_NearZ_Get);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_ViewingFrustumParamerter_FarZ_Get);
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142BC0);
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142BD0);
 
-INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", HH_ClassWrapper_Float_Clamp);
+float HH_ClassWrapper_Float_Clamp(float Src_Value, float Min, float Max) {
+    return float_min(float_max(Src_Value, Min), Max);
+}
 
 INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_wrapper", func_00142C00);
 
